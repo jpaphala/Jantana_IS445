3b8482052debd05f803761bd8d8253b5404584fd
Jpaphala <jpaphala@yahoo.com>
2013-03-17 23:42:05 -0700

    HW5

--- JavaScript1.js
+++ JavaScript1.js
@@ -9,7 +9,7 @@ var x = 0.00;
 if (isNaN(entered)) {
     alert('You must enter a valid number between 2 and 10!');
 } else if (entered <= 1 || entered >= 11) {
-    alert('Your input is ' + entered + '. The vailid input is between 2 and 10.  Please reload the page and try again.');
+    alert('Your input is ' + entered + '. The valid input is between 2 and 10.  Please reload the page and try again.');
 }
 else document.writeln(str + entered +"<br />" );
 
@@ -21,5 +21,16 @@ while (x > .000001)
     x /= 2;
     count++;
 }
-document.write("<br />" + str2 + entered + str3 + count);
+document.write("<br />" + str2 + entered + str3 + count+ "<br />");
+ 
 
+var star = "<div>"
+var lineNumber = number
+for (var line = 0; line < lineNumber; line++) {
+    for (var j = lineNumber - line; j > 0; j--) {
+        star += "*"
+    }
+
+    star += "<br>"
+}
+document.writeln(star + "</div>")
